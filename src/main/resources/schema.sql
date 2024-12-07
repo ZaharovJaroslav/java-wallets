ee69c9e8a45be8d36490d422cc6dd4ae0bd51a3b
Jaroslav <120812496+Yarik-Yarik@users.noreply.github.com>
2024-12-07 17:50:43 +0500

    feat: Добавить миграции для базы данных liquibase

--- src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS wallets;
-
-CREATE TABLE IF NOT EXISTS wallets
- (
-    wallet_id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    wallet_balance    DOUBLE PRECISION     NOT NULL
-);
